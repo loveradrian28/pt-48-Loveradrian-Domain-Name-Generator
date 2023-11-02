2d55327a435b078d4462434c9b652a699a9f0dd6
Adrian Lovera <105324331+loveradrian28@users.noreply.github.com>
2023-11-02 18:43:25 +0000

    DomainName Generator Final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("button").onclick = generateDomainName;
 };
+
+function generateDomainName() {
+  let wordFromUser = document.querySelector("#userWord").value;
+  const domainNameOptions = [
+    "awesome.com",
+    "great.com",
+    "miAmorConTeQuiero.com",
+    "OMG.com"
+  ];
+
+  let finalDomainName = domainNameOptions.map(userWord => {
+    return `${wordFromUser}${userWord}`;
+  });
+
+  let domainNameList = document.querySelector("#DomainNameList");
+  domainNameList.innerHTML = finalDomainName.join("<br>");
+}
